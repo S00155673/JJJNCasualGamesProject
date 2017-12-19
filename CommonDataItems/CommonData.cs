f7a8f0f498c7b760e96dc5019da0977498d594e1
Jordan Davies <S00155673@mail.itsligo.ie>
2017-12-19 16:48:58 +0000

    Initial Commit
    Project Created,
    Added
    - CommonDataItems
       -CommonData.cs
         (with Player Variavbles)
    - MonoGameClient
       -Game1.cs
        (Added Initialize Server,
        ServerConnection State)
    -SignalrGameServer
      -Startup.cs
    
    Referances added for each.

---
+++ CommonDataItems/CommonData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonDataItems
+{
+    public class Position
+    {
+        public int X;
+        public int Y;
+    }
+
+    public class PlayerData
+    {
+        public string playerID;
+        public string imageName = string.Empty;
+        public string GamerTag = string.Empty;
+        public string PlayerName = string.Empty;
+        public int XP;
+        public Position playerPosition;
+        public string Password;
+
+
+
+
+
+
+
+    }
+}
---
+++ MonoGameClient/Game1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameClient
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        string connectionMessage = string.Empty;
+
+
+        // The Signalr Client objects
+        HubConnection serverConnection;
+        IHubProxy proxy;
+
+        public bool Connected { get; set; }
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            serverConnection = new HubConnection("http://localhost:12719/");
+            serverConnection.StateChanged += ServerConnection_StateChanged;
+            proxy = serverConnection.CreateHubProxy("GameHub");
+            serverConnection.Start();
+            base.Initialize();
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        private void ServerConnection_StateChanged(StateChange State)
+        {
+            switch (State.NewState)
+            {
+                case ConnectionState.Connected:
+                    connectionMessage = "Connected...";
+                    Connected = true;
+                    //startGame();
+                    break;
+                case ConnectionState.Disconnected:
+                    connectionMessage = "Disconnected...";
+                    if (State.OldState == ConnectionState.Connected)
+                        connectionMessage = "Lost connection...";
+                    Connected = false;  
+                    break;
+                case ConnectionState.Connecting:
+                    connectionMessage = "Connecting...";
+                    Connected = false;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
